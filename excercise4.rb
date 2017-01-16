97f98b1032fbc74fc1b9eca0747cfa2e1da6b236
Amena <ame1.khatun@gmail.com>
2017-01-16 18:40:28 -0500

    excercise 4 complete

---
+++ excercise4.rb
@@ -0,0 +1,28 @@
+(1..100).each do |n|
+    # n += 1
+    # puts n+1
+
+  if n%3 == 0 && n%5 == 0
+    puts "bitmaker"
+
+  elsif n%3 == 0
+    puts "bit"
+
+  elsif n%5 == 0
+    puts "maker"
+
+
+  else
+    puts n
+  end
+
+end
+
+
+
+  #
+  # if x*3
+  #   puts "bit"
+  # else
+  #   puts "#{number} is negative"
+  # end
